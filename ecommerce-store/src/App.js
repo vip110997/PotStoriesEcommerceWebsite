2ed37cb0f5881d7c204b202a3db45ffeff176361
vip110997 <vishwa.vip.punchihewa@gmail.com>
2019-12-04 09:19:53 +0530

    Added node module and sass stylings

---
+++ ecommerce-store/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">Heading</div>
+        <div className="col-6">
+          <span>
+            <i className="fas fa-home"/>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
